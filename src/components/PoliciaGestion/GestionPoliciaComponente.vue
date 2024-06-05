7677b355a9fea7d6976e0a699a01b8aed3a8ffbd
17diego15 <157295316+17diego15@users.noreply.github.com>
2024-06-05 19:50:18 +0200

    Auditoria hecho falta usuario

--- src/components/PoliciaGestion/GestionPoliciaComponente.vue
+++ src/components/PoliciaGestion/GestionPoliciaComponente.vue
@@ -1,12 +1,14 @@
 <script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue';
+import { defineComponent, ref, reactive, onMounted } from 'vue';
 import { useListadoPolicias } from '@/stores/storePolicia';
 import { useListadoRangos } from '@/stores/storeRango';
+import { useListadoAuditorias } from '@/stores/storeAuditoria';
 
 export default defineComponent({
   setup() {
     const { cargarDatosPolicias, infoPolicias, actualizarPolicia } = useListadoPolicias();
     const { cargarDatosRangos, infoRangos } = useListadoRangos();
+    const { guardarAuditoria } = useListadoAuditorias();
 
     onMounted(() => {
       cargarDatosPolicias();
@@ -14,16 +16,25 @@ export default defineComponent({
     });
 
     const editMode = ref<{ [key: number]: boolean }>({});
+    const rangosOriginales = reactive<{ [key: number]: string }>({});
 
     const startEdit = (idPolicia: number) => {
       editMode.value[idPolicia] = true;
+      const policia = infoPolicias.find(p => p.idPolicia === idPolicia);
+      if (policia && policia.rango) {
+        rangosOriginales[idPolicia] = policia.rango.nombre;
+      }
     };
 
     const saveChanges = async (policia: any) => {
+      const rangoAntiguo = rangosOriginales[policia.idPolicia] || 'Sin Rango';
+
       if (policia.rango && policia.rango.nombre) {
         const selectedRango = infoRangos.find(rango => rango.nombre === policia.rango.nombre);
-        if (selectedRango) {
+        if (selectedRango && selectedRango.nombre !== rangoAntiguo) {
           policia.rango.idRango = selectedRango.idRango;
+        } else {
+          return;
         }
       }
 
@@ -36,7 +47,21 @@ export default defineComponent({
       };
 
       await actualizarPolicia(policiaPostDTO);
+
+      if (rangoAntiguo !== policia.rango.nombre) {
+        const auditoria = {
+          idAuditoria: 0,
+          titulo: "Cambio de Rango",
+          descripcion: `El rango del policía con ID ${policia.idPolicia} ha sido cambiado del rango ${rangoAntiguo} a ${policia.rango.nombre}.`,
+          fecha: new Date(),
+          idPolicia: policia.idPolicia,
+        };
+
+        await guardarAuditoria(auditoria);
+      }
+
       editMode.value[policia.idPolicia] = false;
+      delete rangosOriginales[policia.idPolicia];
     };
 
     return {
@@ -51,50 +76,48 @@ export default defineComponent({
 </script>
 
 <template>
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Foto</th>
-          <th>Nombre</th>
-          <th>Rango</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="policia in infoPolicias" :key="policia.idPolicia">
-          <td>{{ policia.idPolicia }}</td>
-          <td><img :src="policia.ciudadano.imagenUrl" alt="Foto del Policía" class="police-photo"></td>
-          <td>{{ policia.ciudadano.nombre }}</td>
-          <td>
-            <template v-if="editMode[policia.idPolicia]">
-              <select v-model="policia.rango.nombre">
-                <option v-for="rango in infoRangos" :key="rango.idRango" :value="rango.nombre">
-                  {{ rango.nombre }}
-                </option>
-              </select>
-            </template>
-            <template v-else>
-              {{ policia.rango ? policia.rango.nombre : 'Sin Rango' }}
-            </template>
-          </td>
-          <td>
-            <template v-if="editMode[policia.idPolicia]">
-              <button @click="saveChanges(policia)">Guardar</button>
-            </template>
-            <template v-else>
-              <button @click="startEdit(policia.idPolicia)">Editar</button>
-            </template>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+  <table>
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Foto</th>
+        <th>Nombre</th>
+        <th>Rango</th>
+        <th>Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="policia in infoPolicias" :key="policia.idPolicia">
+        <td>{{ policia.idPolicia }}</td>
+        <td><img :src="policia.ciudadano.imagenUrl" alt="Foto del Policía" class="police-photo"></td>
+        <td>{{ policia.ciudadano.nombre }}</td>
+        <td>
+          <template v-if="editMode[policia.idPolicia]">
+            <select v-model="policia.rango.nombre">
+              <option v-for="rango in infoRangos" :key="rango.idRango" :value="rango.nombre">
+                {{ rango.nombre }}
+              </option>
+            </select>
+          </template>
+          <template v-else>
+            {{ policia.rango ? policia.rango.nombre : 'Sin Rango' }}
+          </template>
+        </td>
+        <td>
+          <template v-if="editMode[policia.idPolicia]">
+            <button @click="saveChanges(policia)">Guardar</button>
+          </template>
+          <template v-else>
+            <button @click="startEdit(policia.idPolicia)">Editar</button>
+          </template>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <style scoped>
-.container{
-
-}
+.container {}
 
 table {
   width: 80%;
@@ -105,7 +128,8 @@ table {
   overflow: hidden;
 }
 
-th, td {
+th,
+td {
   border: 1px solid var(--colorFondoCiudadano2);
   padding: 12px;
   text-align: left;
@@ -161,6 +185,4 @@ select option {
   object-fit: cover;
   border: 2px solid #4a90e2;
 }
-
-
 </style>
